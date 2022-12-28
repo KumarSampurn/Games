77f4b2b20f2332031944f486691517c9b14231c4
Epsilon <kumarsampurn@outlook.com>
2022-12-28 22:23:17 +0530

    Functional Website created

--- wach-a-mole/app.js
+++ wach-a-mole/app.js
@@ -1,5 +1,4 @@
 const gridDisplay = document.querySelector('#grid')
-
 function createHoles(){
 
     for(let i =0 ; i < 9 ;i ++)
@@ -13,28 +12,27 @@ function createHoles(){
 createHoles()
 
 
+
 const squares = document.querySelectorAll(".square")
 const mole = document.querySelector(".mole")
 const timeleft = document.querySelector("#time-left")
-const score = document.querySelector("#score")
+const heading = document.querySelector("#score")
+
 
 let result =0
-
+let currentTime=10
 
 function randomSquare(){
     squares.forEach(square=>{
         square.classList.remove("mole")
     })
-
     let randomSquare= squares[Math.floor(Math.random()*9)]
     randomSquare.classList.add("mole")
-
 }
 
 squares.forEach(square=>{
     square.addEventListener("mousedown",checkMatch)
 })
-
 
 function checkMatch(){
     
@@ -43,24 +41,29 @@ function checkMatch(){
         result++;
         
     }
-    score.textContent=result
+    heading.innerHTML="<h1> Score : "+ result + "</h1>"
 }
+
+
 let timerId=null
 function moveMole(){
-    timeId =null
     timerId=setInterval(randomSquare,550)
 }
 
 
-currentTime=10
 
 function displayScore(){
-    console.log("Final Score is : " + result)
+    heading.textContent= "Game Over"
+    gridDisplay.innerHTML=""
+    gridDisplay.innerHTML="<h1> Your Score : "+ result+ "</h1><h6>Refresh to Start</h6>"
 }
 
+
+
+let countDownTimerId =null
 function countDown(){
     currentTime--;
-    timeleft.textContent=currentTime;
+    timeleft.innerHTML="<h1>Time Left :" + currentTime + "</h1>";
     if(currentTime<1)
     {
         displayScore()
@@ -68,9 +71,6 @@ function countDown(){
         clearInterval(timerId)
     }
 }
-let countDownTimerId =null
-
-
 
 let startButton = document.getElementById("start")
 startButton.addEventListener("click",()=>{
